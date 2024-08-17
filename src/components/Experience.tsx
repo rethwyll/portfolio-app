47d178cd4fabb7f0be604b6d2265e3c070f17eba
binghamverses <97802740+binghamverses@users.noreply.github.com>
2024-08-17 13:23:06 -0700

    skills filter

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import data from "../data/experience.json";
+
+// components
 import ExperienceItemDisplay from "./ExperienceItemDisplay";
 
-const Experience = () => {
+// stores
+import { useSkillsStore } from "../stores/skillsStore";
+import { intersection, isEmpty } from "lodash";
+
+const Experience = (): ReactElement | null => {
+  const currentSkills = useSkillsStore(state => state.skills);
+
   return (
     <section>
       <header>
         <h3>Professional Experience</h3>
       </header>
-      {data.map(d => (
-        <ExperienceItemDisplay key={d.name} experienceItem={d} />
-      ))}
+      {data.map(d =>
+        isEmpty(currentSkills) ||
+        !!intersection(d.skills, currentSkills).length ? (
+          <ExperienceItemDisplay key={d.name} experienceItem={d} />
+        ) : null
+      )}
     </section>
   );
 };
