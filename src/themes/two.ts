addb630c0c750e52d22165db38d7bdc4fb256173
binghamverses <97802740+binghamverses@users.noreply.github.com>
2024-08-18 14:02:18 -0700

    theme font sizing

--- src/themes/two.ts
+++ src/themes/two.ts
@@ -31,33 +31,43 @@ export default createTheme({
   typography: () => ({
     fontFamily: "Alfa Slab One, serif",
     h1: {
-      fontSize: "5rem",
-      color: "#bada55"
+      color: "#bada55",
+      fontSize: "5em"
     },
     h2: {
-      fontSize: "20rem",
-      color: "#bada55"
+      color: "#bada55",
+      fontSize: "10em"
     },
     h3: {
-      color: "#c0ffee"
+      color: "#c0ffee",
+      fontSize: "6em"
     },
     h4: {
-      color: "#c0c"
+      color: "#c0c",
+      fontSize: "4em"
     },
     h5: {
-      color: "#c0c"
+      color: "#c0c",
+      fontSize: "2em"
     },
     h6: {
-      color: "#c0c"
+      color: "#c0c",
+      fontSize: "1em"
     },
     body1: {
-      color: "#666"
+      color: "#333",
+      fontSize: "3rem"
     }
   }),
   components: {
     MuiButton: {
       defaultProps: {
         variant: "contained"
+      },
+      styleOverrides: {
+        root: {
+          fontSize: "1em"
+        }
       }
     }
   }
