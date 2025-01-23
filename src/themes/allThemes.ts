c3b51c3da020a90d3c9ac8b30e0e59ef0cd5895e
rethwyll <aprilbingham@MacBook-Pro.local>
2025-01-23 13:18:47 -0800

    commenting print

--- src/themes/allThemes.ts
+++ src/themes/allThemes.ts
@@ -1,7 +1,7 @@
 import { Theme } from "@mui/material/styles";
 import bluecalm from "./bluecalm";
 // import paper from "./paper";
-import print from "./print";
+// import print from "./print";
 import yellowstack from "./yellowstack";
 
-export default { bluecalm, yellowstack, print } as { [key: string]: Theme };
+export default { bluecalm, yellowstack } as { [key: string]: Theme };
